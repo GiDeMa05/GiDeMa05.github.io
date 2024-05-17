681b9b0e8b3b435423061fd5e6392c241b84870c
GiDeMa05 <giovannidemattia005@gmail.com>
2024-05-17 16:37:00 +0200

    Add files via upload

---
+++ scripts.js
@@ -0,0 +1,12 @@
+function openNav() {
+    document.getElementById("mySidenav").style.width = "300px";
+    document.getElementById("main").style.marginLeft = "300px";
+    document.body.style.backgroundColor = "rgba(0,0,0,0.4)";
+}
+
+function closeNav() {
+    document.getElementById("mySidenav").style.width = "0";
+    document.getElementById("main").style.marginLeft= "0";
+    button_function(this)
+    document.body.style.backgroundColor = "rgba(0, 0, 0, 0)";
+}
