64d8d36e6cce9f042b1c3b4ffac435c36e145760
GiDeMa05 <giovannidemattia005@gmail.com>
2024-05-18 21:29:18 +0200

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -1,7 +1,11 @@
 function openNav() {
+    if(window.innerWidth>600){
     document.getElementById("mySidenav").style.width = "300px";
     document.getElementById("main").style.marginLeft = "300px";
-    document.body.style.backgroundColor = "rgba(0,0,0,0.4)";
+    }else{
+        document.getElementById("mySidenav").style.width = "300px";
+        document.getElementById("main").style.marginLeft = "300px";
+    }
 }
 
 function closeNav() {
